42e9ec0a738cfc12cf80918c7c9280bf35ee7532
DearestAtivie <dear.ativie@gmail.com>
2023-03-11 21:42:42 -0600

    Made the favorite key have a property of yes

--- scripts/albums.js
+++ scripts/albums.js
@@ -11,7 +11,7 @@ let albums = [{
   artist: 'Kid Cudi',
   genre: 'Hip-Hop',
   artwork: 'img/kidcudi.jpg',
-  favorite: yes
+  favorite,
 },
 {
   name: 'positions',
@@ -32,7 +32,7 @@ let albums = [{
   artist: 'Rihanna',
   genre: 'R&B/Pop',
   artwork: 'img/rihanna.jpeg',
-  favorite: yes
+  favorite,
 },
 {
   name: 'Days Are Gone',
@@ -45,7 +45,7 @@ let albums = [{
   artist: 'Drake',
   genre: 'Hip-Hop',
   artwork: 'img/drake.jpeg',
-  favorite: yes,
+  favorite,
 },
 {
   name: "Based On A T.R.U. Story",
