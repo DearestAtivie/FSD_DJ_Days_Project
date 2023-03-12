c8ec856e0e285e4b2764a4dbcf945a26aff36802
DearestAtivie <dear.ativie@gmail.com>
2023-03-11 21:50:57 -0600

    Added the favorite: key to the first html code on albums. js

--- scripts/albums.js
+++ scripts/albums.js
@@ -11,7 +11,7 @@ let albums = [{
   artist: 'Kid Cudi',
   genre: 'Hip-Hop',
   artwork: 'img/kidcudi.jpg',
-  favorite,
+  favorite: 'yes',
 },
 {
   name: 'positions',
@@ -32,7 +32,7 @@ let albums = [{
   artist: 'Rihanna',
   genre: 'R&B/Pop',
   artwork: 'img/rihanna.jpeg',
-  favorite,
+  favorite: 'yes',
 },
 {
   name: 'Days Are Gone',
@@ -45,7 +45,7 @@ let albums = [{
   artist: 'Drake',
   genre: 'Hip-Hop',
   artwork: 'img/drake.jpeg',
-  favorite,
+  favorite: 'yes',
 },
 {
   name: "Based On A T.R.U. Story",
